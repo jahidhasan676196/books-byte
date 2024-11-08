38d1670360f32ddb053127c428e922cd3a695c4d
Jahidhasan71 <rafsanjahid1221@gmail.com>
2024-11-08 12:45:31 +0600

    added the home and card section

--- src/Component/Books/Books.jsx
+++ src/Component/Books/Books.jsx
@@ -1,9 +1,15 @@
+import Book from "../Book/Book";
 
 
-const Books = () => {
+const Books = ({books}) => {
     return (
-        <div>
-            
+        <div className="mt-24 text-center">
+            <h3 className="text-[#131313] font-bold text-4xl mb-9">Books</h3>
+            <div className="grid grid-cols-3 gap-6">
+            {
+                books.map(book=><Book book={book}></Book>)
+            }
+            </div>
         </div>
     );
 };
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Banner from '../Banner/Banner';
+import { useLoaderData } from 'react-router-dom';
+import Books from '../Books/Books';
 
 const Home = () => {
+    const books=useLoaderData()
+
     return (
         <div>
             <Banner></Banner>
+            <Books books={books}></Books>
         </div>
     );
 };
