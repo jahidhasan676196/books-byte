7ea09c7c363a19e8d1e42b839c1b703aae0af7c5
Jahidhasan71 <rafsanjahid1221@gmail.com>
2024-11-08 16:07:18 +0600

    added the home section

--- src/Component/Book/Book.jsx
+++ src/Component/Book/Book.jsx
@@ -1,19 +1,29 @@
-
+import { MdOutlineStarRate } from "react-icons/md";
 
 const Book = ({book}) => {
-    const{image}=book
+    const{image,book_name,author,category,tags,rating}=book
     return (
-        <div className="card bg-base-100  shadow-xl">
+        <div className="card border text-start p-6 space-y-4">
         <figure>
           <img
+          className="rounded-2xl"
             src={image}
-            alt="Shoes" />
+            alt="" />
         </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+        <div className="space-y-4">
+        <div className="flex gap-4 text-[#23BE0A] font-medium text-base">
+          <p>#{tags[0]}</p>
+          <p>#{tags[1]}</p>
+        </div>
+          <h2 className="text-2xl font-bold text-[#131313]">{book_name}</h2>
+          <p className="text-base font-medium text-[#131313CC]">By: {author}</p>
+          <hr />
+          <div className="flex justify-between text-base font-medium text-[#131313CC] ">
+            <p>{category}</p>
+            <div className="flex items-center justify-center">
+              <p>{rating}</p>
+              <MdOutlineStarRate className="w-5"></MdOutlineStarRate>
+            </div>
           </div>
         </div>
       </div>
